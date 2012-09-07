22d00903128370f8561a42f212b4aafab6f23dfe
Michael B. Price <michael.b.price.dev@gmail.com>
2012-09-07 08:59:45 -0700

    Copying array elements over now... hell yeah.

--- array_splice/array_splice.cpp
+++ array_splice/array_splice.cpp
@@ -5,21 +5,63 @@
  
 using namespace std;
  
-template <size_t ... All>
-struct const_sum { };
- 
 template <size_t Value, size_t ... Rest>
-struct const_sum<Value, Rest...>
+struct const_sum
 {
-    static const size_t value = Value + const_sum<Rest...>::value;
+    static constexpr size_t value = Value + const_sum<Rest...>::value;
 };
  
 template <size_t Value>
 struct const_sum<Value>
 {
-    static const size_t value = Value;
+    static constexpr size_t value = Value;
+};
+
+template <size_t IndicesToGo, size_t Value, size_t ... Rest>
+struct variadic_index
+{
+    static constexpr size_t value = variadic_index<IndicesToGo - 1, Rest...>::value;
+};
+
+template <size_t Value, size_t ... Rest>
+struct variadic_index<0, Value, Rest...>
+{
+    static constexpr size_t value = Value;
 };
  
+
+template <size_t TupleIndex, size_t StartOffset, size_t IndicesLeft>
+struct array_to_tuple
+{
+    template <size_t ... Slices, typename T, size_t N>
+    static void copy (std::tuple<std::array<T, Slices>...> & t, const std::array<T, N> & a)
+    {
+        auto & dest = std::get<TupleIndex>(t);
+
+        constexpr size_t Left = StartOffset;
+        constexpr size_t Right = StartOffset + variadic_index<TupleIndex, Slices...>::value;
+
+        std::copy(a.begin() + Left, a.begin() + Right, dest.begin());
+
+        array_to_tuple<TupleIndex + 1, Right, IndicesLeft - 1>::copy(t, a);
+    }
+};
+
+template <size_t TupleIndex, size_t StartOffset>
+struct array_to_tuple<TupleIndex, StartOffset, 1>
+{
+    template <size_t ... Slices, typename T, size_t N>
+    static void copy (std::tuple<std::array<T, Slices>...> & t, const std::array<T, N> & a)
+    {
+        auto & dest = std::get<TupleIndex>(t);
+
+        constexpr size_t Left = StartOffset;
+        constexpr size_t Right = StartOffset + variadic_index<TupleIndex, Slices...>::value;
+
+        std::copy(a.begin() + Left, a.begin() + Right, dest.begin());
+    }
+};
+
  
 template <size_t ... Slices, typename T, size_t N>
 std::tuple<std::array<T, Slices>...> array_splice (const std::array<T, N> & a)
@@ -28,7 +70,10 @@ std::tuple<std::array<T, Slices>...> array_splice (const std::array<T, N> & a)
  
     std::tuple<std::array<T, Slices>...> t;
  
+    constexpr size_t splice_count = sizeof...(Slices);
  
+    array_to_tuple<0, 0, splice_count>::copy(t, a);
+
     return t;
 }
  
@@ -36,33 +81,24 @@ int main ()
 {
     std::array<int, 10> a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     
-    for (int * e = a.begin(); e != a.end(); ++e) { cout << *e << " "; } cout << endl << endl;
+    for (auto e : a) { cout << e << " "; } cout << endl << endl;
  
-    std::tuple<std::array<int,2>,
-               std::array<int,3>,
-               std::array<int,2>,
-               std::array<int,2>,
-               std::array<int,1>> t = array_splice<2, 3, 2, 2, 1>(a);
+    auto t = array_splice<2, 3, 2, 2, 1>(a);
  
-    cout << std::get<0>(t).size() << ": ";
-    for (int * e = std::get<0>(t).begin(); e != std::get<0>(t).end(); ++e)
-    { cout << *e << " "; } cout << endl;
+    cout << get<0>(t).size() << ": ";
+    for (auto e : get<0>(t)) { cout << e << " "; } cout << endl;
  
-    cout << std::get<1>(t).size() << ": ";
-    for (int * e = std::get<1>(t).begin(); e != std::get<1>(t).end(); ++e)
-    { cout << *e << " "; } cout << endl;
+    cout << get<1>(t).size() << ": ";
+    for (auto e : get<1>(t)) { cout << e << " "; } cout << endl;
  
-    cout << std::get<2>(t).size() << ": ";
-    for (int * e = std::get<2>(t).begin(); e != std::get<2>(t).end(); ++e)
-    { cout << *e << " "; } cout << endl;
+    cout << get<2>(t).size() << ": ";
+    for (auto e : get<2>(t)) { cout << e << " "; } cout << endl;
  
-    cout << std::get<3>(t).size() << ": ";
-    for (int * e = std::get<3>(t).begin(); e != std::get<3>(t).end(); ++e)
-    { cout << *e << " "; } cout << endl;
+    cout << get<3>(t).size() << ": ";
+    for (auto e : get<3>(t)) { cout << e << " "; } cout << endl;
  
-    cout << std::get<4>(t).size() << ": ";
-    for (int * e = std::get<4>(t).begin(); e != std::get<4>(t).end(); ++e)
-    { cout << *e << " "; } cout << endl << endl;
+    cout << get<4>(t).size() << ": ";
+    for (auto e : get<4>(t)) { cout << e << " "; } cout << endl << endl;
  
     cout << "Done" << endl;
  
