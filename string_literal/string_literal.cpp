4543663294cbf34decee671da3215b54b345f5da
Michael Price <michael.b.price.dev@gmail.com>
2013-12-11 14:46:31 -0600

    More functions + cleanup

--- string_literal/string_literal.cpp
+++ string_literal/string_literal.cpp
@@ -1,8 +1,9 @@
-//#include <array>
+
+char * gets(char *); // works around silly C++11/14 library issue on ToT clang
+
 #include <iostream>
 #include <ostream>
 #include <string>
-//#include <cstddef>
 
 template <char... Chars> struct literal_string
 {
@@ -21,19 +22,29 @@ template <char... Chars> struct literal_string
         return size() == 0;
     }
 
-    // TODO: operator[]
+    constexpr char operator[] (size_t pos) const
+    {
+        static_assert(sizeof...(Chars) > 0, "operator[] is not valid on an empty string");
+        return _data[pos];
+    }
 
     template <size_t Index>
     constexpr char at () const
     {
-        static_assert(sizeof...(Chars) > 0, "at() is not valid on an empty string");
+        static_assert(sizeof...(Chars) > 0, "at<Index>() is not valid on an empty string");
         static_assert(Index < sizeof...(Chars), "Invalid index");
         return _data[Index];
     }
 
+    constexpr char at (size_t pos) const
+    {
+        static_assert(sizeof...(Chars) > 0, "at() is not valid on an empty string");
+        return _data[pos];
+    }
+
     constexpr char back () const
     {
-        static_assert(sizeof...(Chars) > 0, "back() is not valid on an empty string")
+        static_assert(sizeof...(Chars) > 0, "back() is not valid on an empty string");
         return _data[sizeof...(Chars) - 1];
     }
 
@@ -56,9 +67,8 @@ template <char... Chars> struct literal_string
     template <char... LookForChars>
     constexpr std::string::size_type find (const literal_string<LookForChars...> & str, std::size_t pos = 0) const
     {
-        static_assert(size() > 0, "find() is not valid for an empty string");
-        static_assert(str.size() > 0, "The parameter to find() cannot be an empty string");
-        // TODO: assert on pos
+        static_assert(sizeof...(Chars) > 0, "find() is not valid for an empty string");
+        static_assert(sizeof...(LookForChars) > 0, "The parameter to find() cannot be an empty string");
 
         for (std::string::size_type i = 0; i + pos < size(); ++i)
         {
@@ -79,7 +89,7 @@ template <char... Chars> struct literal_string
         
         return std::string::npos;
     }
-    
+ 
     // TODO: What about const char* form of find()
 
     constexpr std::string::size_type find (char ch, size_t pos = 0) const
@@ -93,7 +103,40 @@ template <char... Chars> struct literal_string
         return std::string::npos;
     }
     
-    // TODO: rfind
+    template <char... LookForChars>
+    constexpr std::string::size_type rfind (const literal_string<LookForChars...> & str, std::size_t pos = sizeof...(Chars)) const
+    {
+        static_assert(sizeof...(Chars) > 0, "find() is not valid for an empty string");
+        static_assert(sizeof...(LookForChars) > 0, "The parameter to find() cannot be an empty string");
+
+        for (/* empty init */; pos > 0; --pos)
+        {
+            for (std::string::size_type j = 0; pos + j < size() && j < str.size(); ++j)
+            {
+                if (_data[pos+j] != str._data[j])
+                    break;
+
+                if (j == str.size() - 1)
+                    return pos;
+            }
+        }
+        
+        return std::string::npos;
+    }
+
+    // TODO: What about const char* form of rfind()
+
+    constexpr std::string::size_type rfind (char ch, size_t pos = sizeof...(Chars)) const
+    {
+        for (/* empty init */; pos > 0; --pos)
+        {
+            if (_data[pos] == ch)
+                return pos;
+        }
+
+        return std::string::npos;
+    }
+
     // TODO: find_first_of
     // TODO: find_last_of
     // TODO: find_first_not_of
@@ -102,7 +145,7 @@ template <char... Chars> struct literal_string
     // TODO: substr
     
     template <char... OtherChars>
-    constexpr int compare (const string_literal<OtherChars...> & other) const
+    constexpr int compare (const literal_string<OtherChars...> & other) const
     {
         std::size_t i = 0;
         
@@ -126,9 +169,11 @@ template <char... Chars> struct literal_string
     
     // TODO: Other forms of compare()
 
+    // Conversions
     std::string to_string() const { return std::string{_data}; }
 
-    //const std::array<char, sizeof...(Chars)> _data = { Chars... };
+    constexpr operator const char* () const { return _data; }
+
     const char _data[sizeof...(Chars) + 1] = { Chars..., '\0' };
 };
 
@@ -147,24 +192,72 @@ inline constexpr auto operator+ (const literal_string<LeftChars...> &l,
 // TODO: relational operators
 
 template <char... Chars>
-inline std::ostream& operator<< (ostream & os, const literal_string<Chars...> & str)
+inline std::ostream& operator<< (std::ostream & os, const literal_string<Chars...> & str)
 {
     os << str.c_str();
     return os;
 }
 
+template <typename Str>
+struct Speaker
+{
+    Speaker() { std::cout << "Unknown" << std::endl; }
+};
+
+template <>
+struct Speaker<decltype("Oh my!"_cs)>
+{
+    Speaker() { std::cout << "George Takei" << std::endl; }
+};
+
+template <>
+struct Speaker<decltype("Engage!"_cs)>
+{
+    Speaker() { std::cout << "Sir Patrick Stewart" << std::endl; }
+};
+
+template <typename Str>
+void speak(const Str &)
+{
+    std::cout << "Function: Unknown" << std::endl;
+}
+
+template <>
+void speak(const decltype("Oh My!"_cs) &)
+{
+    std::cout << "Function: George Takei" << std::endl;
+}
+
 int main() 
 {
-    constexpr auto c = "Hello "_cs + "World!"_cs + " John"_cs;
+    constexpr auto c = "Hello "_cs + "World!"_cs;
 
-    //for (auto x : c._data)
-    //  printf("%c", x);
     std::cout << c << std::endl; 
 
+    static_assert(c.find('?') == std::string::npos, "I'm so so sorry");
     static_assert(c.at<11>() == '!', "Oh my!");
-    static_assert(c.find_first_of(' ') == 5, "Say what!");
+    static_assert(c[10] == 'd', "Crapola");
+    static_assert(c.find(' ') == 5, "Say what!");
+    static_assert(c.find('o') == 4, "No good");
+    static_assert(c.rfind('o') == 7, "Ooops");
+    static_assert(c.find("orl"_cs) == 7, "Holla");
+    static_assert(c.rfind("el"_cs) == 1, "Ooops");
 
     auto s = c.to_string();
 
+    s = "I don't think so";
+    std::cout << s << std::endl;
+
+    const char * old = "My word!"_cs;
+    std::cout << old << std::endl;
+
+    auto g = Speaker<decltype("Oh my!"_cs)>{};
+    auto p = Speaker<decltype("Engage!"_cs)>{};
+    auto b = Speaker<decltype("Resistance is futile"_cs)>{};
+
+    speak("My name is Neo"_cs);
+    speak("Oh My!"_cs);
+
     return 0;
 }
+
